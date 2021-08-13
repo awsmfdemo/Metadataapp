a97c3ad271603452eb4e4408c92d1d7a93c62a66
Abdul Waheed <m.abdulwahid@gmail.com>
2021-08-13 17:42:29 +0530

    new

--- Server.js
+++ Server.js
@@ -19,6 +19,22 @@ app.get('/', function(req, res) {
 
   var InstanceId = "";
 
+  Q.all([
+    metadata.getMetadataForInstance('ami-id'),
+    metadata.getMetadataForInstance('hostname'),
+    metadata.getMetadataForInstance('public-hostname'),
+    metadata.getMetadataForInstance('public-ipv4'),
+])
+.spread(function(amiID, hostname, publicHostname, publicIPv4) {
+    console.log("AMI-ID: " + amiID);
+    console.log("Hostname: " + hostname);
+    console.log("Public Hostname: " + publicHostname);
+    console.log("Public IPv4: " + publicIPv4);
+})
+.fail(function(error) {
+    console.log("Error: " + error);
+});
+
   metadata.getMetadataForInstance('instance-id')
   .then(function(instanceId) {
       console.log("Instance ID: " + instanceId);
