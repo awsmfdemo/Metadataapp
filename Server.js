678a8b53fd05036d1221165022ce99e792cfb538
Abdul Waheed <m.abdulwahid@gmail.com>
2021-08-13 17:31:09 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var metadata = require('node-ec2-metadata');
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/images'));
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  var InstanceId = "";
+
+  metadata.getMetadataForInstance('instance-id')
+  .then(function(instanceId) {
+      console.log("Instance ID: " + instanceId);
+      InstanceId = instanceId;
+  })
+  .fail(function(error) {
+      console.log("Error: " + error);
+  });
+
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline,
+    Instanceid: InstanceId
+  });
+});
+
+// about page
+app.get('/about', function(req, res) {
+  
+  res.render('pages/about');
+});
+
+
+ 
+metadata.getMetadataForInstance('instance-id')
+.then(function(instanceId) {
+    console.log("Instance ID: " + instanceId);
+})
+.fail(function(error) {
+    console.log("Error: " + error);
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
