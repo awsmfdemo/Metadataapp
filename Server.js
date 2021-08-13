cbc6673b2190d068240e33cbd9752ea501e42af2
Abdul Waheed <m.abdulwahid@gmail.com>
2021-08-13 22:38:34 +0530

    new

--- Server.js
+++ Server.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res) {
   const mac = metadata.getMetadataForInstance('mac');
   const iaminfo = metadata.getMetadataForInstance('iam/info');
 
-  var args = [ '01-23-45-67-89-ab', instanceid ];
+  var args = [ mac, publicipv4];
   const privateip = metadata.getMetadataForInstance('network/interfaces/macs/mac/ipv4-associations/public-ip', args);
 
   /*const amiid = "ami id";
